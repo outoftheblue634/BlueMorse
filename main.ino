4906f575c9b3c7da555bc15315d29697a42a59f0
djderepentigny <noahsito2@gmail.com>
2021-04-15 22:29:55 -0400

    test noah

--- main.ino
+++ main.ino
@@ -30,7 +30,6 @@ int state = 0;
 int unit_delay = 125;
 int min_delay = 5;
 
-// test Branch
 void setup() {
   Serial.begin(9600);
   BLE.begin(9600);
@@ -168,4 +167,5 @@ int i = 0;
   }
 
   code = "";
+  //comment 2
 }
