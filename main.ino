c2c1d7708ce3150f6ef21e24d4aa3b5555e5a43a
djderepentigny <noahsito2@gmail.com>
2021-04-15 22:49:08 -0400

    new comment

--- main.ino
+++ main.ino
@@ -1,4 +1,4 @@
-#include <Wire.h>
+#include <Wire.h> //wired
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <SoftwareSerial.h>
@@ -167,5 +167,5 @@ int i = 0;
   }
 
   code = "";
-  //comment 2
+
 }
